ba15e15d23bb98e4de02d22da59c54a3af13aa20
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-04-18 12:11:27 +0530

    feat: add metadata

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "ruru-dev07",
+  url: "https://ruru-dev07.vercel.app",
+  ogImage: "https://ruru-dev07.vercel.app/og.png",
+  description:
+    "Skilled in both front-end and back-end development, I bring websites to life with a focus on user experience and technical excellence.",
+  links: {
+    github: "https://github.com/ruru-m07",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
