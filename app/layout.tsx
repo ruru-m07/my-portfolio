7cd48285b68d95f59b77481773e1bdaf805d4322
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-04-18 12:11:27 +0530

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,22 +2,47 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider/theme-provider";
+import { siteConfig } from "@/config/site";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ruru: Full-Stack Web Developer",
-  description:
-    "Skilled in both front-end and back-end development, I bring websites to life with a focus on user experience and technical excellence.",
-  creator: "Ruru",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords:
+    "web development, full-stack, front-end, back-end, ruru, ruru-m07, ruru.dev07, ruru-dev",
   authors: [
     {
       name: "Ruru",
       url: "https://github.com/ruru-m07/",
     },
   ],
-  keywords:
-    "web development, full-stack, front-end, back-end, user experience, technical excellence",
+  creator: "Ruru",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon_io/favicon.ico",
+    shortcut: "/favicon_io/favicon-16x16.png",
+    apple: "/favicon_io/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +52,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="NyaIMHJt9--uZGUgzJpGQPNcUJtVLtvaEkodqOuJ7-I"
+        />
+      </head>
       <body className={`${inter.className} bg-primary-foreground/35 `}>
         <ThemeProvider
           attribute="class"
