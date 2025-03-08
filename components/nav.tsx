34c4b107ecf336ea566e1870021e3099df82ff10
ruru <ruru.dev07@gmail.com>
2025-03-08 15:48:47 +0530

    new stuff

---
+++ components/nav.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { cn } from "@/lib/cn";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+
+export function Nav() {
+  return (
+    <nav className="flex items-center gap-3 mb-4 max-w-[600px] mx-auto">
+      <Link href="/" className="mr-auto">
+        <Image
+          alt="Me"
+          src="/me.png"
+          width="40"
+          height="40"
+          className="size-8 rounded-full"
+        />
+      </Link>
+      <NavLink href="/blog">Blog</NavLink>
+    </nav>
+  );
+}
+
+export function NavLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const active = pathname === href || pathname.startsWith(href + "/");
+  const linkRef = useRef<HTMLAnchorElement>(null);
+  const [width, setWidth] = useState<string | number>("100%");
+
+  useEffect(() => {
+    if (!linkRef.current) return;
+
+    setWidth(linkRef.current.clientWidth);
+  }, []);
+
+  return (
+    <Link
+      ref={linkRef}
+      href={href}
+      className={cn(
+        "relative text-neutral-400 text-sm transition-colors hover:text-neutral-200",
+        active && "text-neutral-50"
+      )}
+    >
+      {children}
+      {active && (
+        <div
+          className="absolute h-px bottom-0 inset-x-0 bg-neutral-200"
+          role="none"
+        />
+      )}
+    </Link>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+import { withContent } from "@fuma-content/next";
+import rehypeShiki from "@shikijs/rehype";
+import remarkGfm from "remark-gfm";
+import remarkHeadingId from "remark-heading-id";
 
-export default nextConfig;
+export default withContent({
+  content: {
+    files: ["./content/**/*"],
+    outputDir: "./.content",
+    mdxOptions: {
+      remarkPlugins: [remarkGfm, [remarkHeadingId, { defaults: true }]],
+      rehypePlugins: [[rehypeShiki, { theme: "github-dark" }]],
+    },
+  },
+});
